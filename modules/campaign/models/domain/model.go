4c715ef86ab76f7e3438682bc1b1b21772516698
terenjit <87969187+terenjit@users.noreply.github.com>
2023-04-28 17:02:33 +0700

    campaign model

--- modules/campaign/models/domain/model.go
+++ modules/campaign/models/domain/model.go
@@ -1,1 +1,27 @@
 package domain
+
+import "time"
+
+type Campaign struct {
+	ID               string    `json:"id"`
+	UserID           string    `json:"user_id"`
+	Name             string    `json:"name"`
+	ShortDescription string    `json:"short_description"`
+	Description      string    `json:"description" `
+	Perks            string    `json:"perks"`
+	BackerCount      int       `json:"backer_count"`
+	GoalAmount       int       `json:"goal_amount"`
+	CurrentAmount    int       `json:"current_amount"`
+	Slug             int       `json:"slug"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+}
+
+type CampaignImage struct {
+	ID         string    `json:"id"`
+	CampaignID string    `json:"campaign_id"`
+	FileName   string    `json:"name"`
+	IsPrimary  int       `json:"is_primary"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
